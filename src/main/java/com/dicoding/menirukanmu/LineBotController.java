9bad5771e7f6b6ccb3c5bb68be4adb3460afae85
bahri <bahri@atmatech.net>
2017-02-07 09:59:25 +0700

    deploy

--- src/main/java/com/dicoding/menirukanmu/LineBotController.java
+++ src/main/java/com/dicoding/menirukanmu/LineBotController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import retrofit2.Response;
 
 import java.io.IOException;
-
+//deploy
 @RestController
 @RequestMapping(value="/linebot")
 public class LineBotController
